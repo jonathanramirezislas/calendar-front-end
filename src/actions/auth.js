5860b1231a3be4f6ddf4506370677cc52fbdf991
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-01-25 18:38:36 -0600

    clean events
    store

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,7 @@
 import { fetchSinToken, fetchConToken } from '../helpers/fetch'
 import { types } from '../types/types';
 import Swal from 'sweetalert2';
+import { eventLogout } from './events';
 
 
 
@@ -83,6 +84,7 @@ export const startLogout = () => {
     return ( dispatch ) => {
 
         localStorage.clear(); 
+        dispatch( eventLogout() );//cleand events from store
         dispatch( logout() );
     }
 }
